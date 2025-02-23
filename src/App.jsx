2e3492a8e6d13c039600ff4b78425c995f08f5f6
ryokf <ryokhrisnaf@gmail.com>
2025-02-23 11:41:51 +0700

    useframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,14 @@
 import './App.css'
 import { Canvas } from '@react-three/fiber'
+import Basic from './Basic'
+import Cube from './Cube'
 
 function App() {
 
   return (
     <div className='h-screen flex justify-center items-center'>
-      {/* 
-        The <Canvas> component from @react-three/fiber is a wrapper that allows you to render 3D content using Three.js in a React application. 
-        It handles the creation and management of the WebGL context and allows you to define your 3D scene using JSX.
-        Canvas component include a camera, renderer, and scene.
-        */}
       <Canvas>
-        {/* 
-          The <mesh> component is a wrapper around the Three.js Mesh object. 
-          It allows you to define a 3D object using JSX and render it in your scene.
-          The mesh component takes two props: geometry and material.
-          The geometry prop defines the shape of the object, and the material prop defines the appearance of the object.
-        */}
-        <mesh position={[2, 2, 0]}>
-          <torusKnotGeometry />
-          <meshNormalMaterial />
-        </mesh>
-        <mesh position={[-2, 2, 0]} rotation={[0, Math.PI / 2, 0]}>
-          <torusKnotGeometry />
-          <meshNormalMaterial />
-        </mesh>
-        {/* 
-          The <group> component is a wrapper around the Three.js Group object. 
-          It allows you to group multiple 3D objects together and treat them as a single entity.
-          The group component takes a single prop: children.
-          The children prop is an array of JSX elements that define the 3D objects that should be grouped together.
-        */}
-        <group position={[0, -2, 0]}>
-          <mesh position={[2, 0, 0]} >
-            <torusKnotGeometry />
-            <meshNormalMaterial />
-          </mesh>
-          <mesh position={[-2, 0, 0]} rotation={[0, Math.PI / 2, 0]}>
-            <torusKnotGeometry />
-            <meshNormalMaterial />
-          </mesh>
-        </group>
+        <Cube></Cube>
       </Canvas>
     </div>
   )
---
+++ src/Cube.jsx
@@ -0,0 +1,25 @@
+import { useFrame } from '@react-three/fiber'
+import React from 'react'
+
+const Cube = () => {
+    const cubeRef = React.useRef()
+
+    // useFrame is a hook that allows you to schedule a function to be called at each frame rendered by the renderer.
+    // The function takes two arguments: state and delta. state is an object containing information about the current state of the renderer, delta is the time in seconds since the last frame.
+    // useFrame is a good place to put code that should be updated every frame, such as updating the rotation of an object.
+    useFrame((state, delta) => {
+        cubeRef.current.rotation.x += delta
+        cubeRef.current.rotation.y += delta
+    })
+
+    return (
+        <group>
+            <mesh ref={cubeRef}>
+                <boxGeometry />
+                <meshNormalMaterial />
+            </mesh>
+        </group>
+    )
+}
+
+export default Cube
